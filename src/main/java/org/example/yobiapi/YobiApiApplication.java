94551b802184440db973b0d89b9c2cfcfc302d5d
JeongSeokHan <wjdtjrrkt8888@gmail.com>
2024-07-15 15:42:28 +0900

    Yobi 프로젝트 생성 및 커스텀 예외 추가

---
+++ src/main/java/org/example/yobiapi/YobiApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.yobiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YobiApiApplication.class, args);
+    }
+
+}
