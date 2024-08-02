311fd20358de3711f6f0e208fd518be0b656d371
JeongSeokHan <wjdtjrrkt8888@gmail.com>
2024-08-02 17:42:12 +0900

    유저, 레시피, 게시판 Entity 추가

---
+++ src/main/java/org/example/yobiapi/user/Entity/User.java
@@ -0,0 +1,63 @@
+package org.example.yobiapi.user.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.yobiapi.user.dto.UserDTO;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, name = "user_id")
+    private String userId;
+
+    @Column(nullable = false, name = "password")
+    private String passWord;
+
+    @Column(nullable = false, name = "nickname")
+    private String nickName;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "phone_number")
+    private String phoneNumber;
+
+    @CreationTimestamp
+    @Column(nullable = false, name = "join_date")
+    private LocalDateTime joinDate;
+
+    @ColumnDefault(("0"))
+    @Column(name = "followers_count")
+    private Integer followersCount;
+
+    @ColumnDefault("0")
+    @Column(name = "following_count")
+    private Integer followingCount;
+
+    @Builder
+    public static User toUser(UserDTO userDTO) {
+        User user = new User();
+        user.setUserId(userDTO.getUserId());
+        user.setPassWord(user.getPassWord());
+        user.setNickName(userDTO.getNickName());
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+
+        return user;
+    }
+
+}
