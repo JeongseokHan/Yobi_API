0eb2f00b7c1f307d521acd2b77b65b33c48966b8
JeongSeokHan <wjdtjrrkt8888@gmail.com>
2024-08-06 14:12:54 +0900

    뷰로그 Entity 추가

---
+++ src/main/java/org/example/yobiapi/view_log/Entity/View_Log.java
@@ -0,0 +1,46 @@
+package org.example.yobiapi.view_log.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.yobiapi.recipe.Entity.Recipe;
+import org.example.yobiapi.user.Entity.User;
+import org.example.yobiapi.view_log.dto.View_LogDTO;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "view_log")
+public class View_Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "log_id")
+    private Integer logId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "recipe_id", referencedColumnName = "recipe_id")
+    private Recipe recipe;
+
+    @CreationTimestamp
+    @Column(name = "view_date")
+    private LocalDateTime viewDate;
+
+    public static View_Log toView_Log(View_LogDTO viewlogDTO) {
+        View_Log viewLog = new View_Log();
+        viewLog.user.setUserId(viewlogDTO.getUserId());
+        viewLog.recipe.setRecipeId(viewlogDTO.getRecipeId());
+
+        return viewLog;
+    }
+}
---
+++ src/main/java/org/example/yobiapi/view_log/dto/View_LogDTO.java
@@ -0,0 +1,13 @@
+package org.example.yobiapi.view_log.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class View_LogDTO {
+    private String userId;
+    private Integer recipeId;
+}
