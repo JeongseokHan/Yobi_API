162301d97d8b1faa306d9a20d308d52d135aa142
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-12 13:44:30 +0900

    커스텀 예외 항목 추가

--- src/main/java/org/example/yobiapi/exception/CustomErrorCode.java
+++ src/main/java/org/example/yobiapi/exception/CustomErrorCode.java
@@ -15,7 +15,15 @@ public enum CustomErrorCode {
     ID_SHORT_REQUEST(HttpStatus.BAD_REQUEST, "ACCOUNT-006", "아이디를 6글자 이상 적어주세요."),
     PW_SHORT_REQUEST(HttpStatus.BAD_REQUEST, "ACCOUNT-007", "비밀번호를 6글자 이상 적어주세요."),
     NICKNAME_SHORT_REQUEST(HttpStatus.BAD_REQUEST, "ACCOUNT-008", "닉네임을 2글자 이상 적어주세요."),
-    NICKNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "ACCOUNT-009", "이미 존재하는 닉네임 입니다.");
+    NICKNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "ACCOUNT-009", "이미 존재하는 닉네임 입니다."),
+    CATEGORY_IS_EMPTY(HttpStatus.BAD_REQUEST, "ACCOUNT-010", "카테고리를 입력해주세요."),
+    CATEGORY_LONG_REQUEST(HttpStatus.BAD_REQUEST, "ACCOUNT-011", "카테고리는 45자 이내로 작성해주세요."),
+    INGREDIENT_IS_EMPTY(HttpStatus.BAD_REQUEST, "ACCOUNT-012", "재료를 입력해주세요."),
+    INGREDIENT_LONG_REQUEST(HttpStatus.BAD_REQUEST, "ACCOUNT-013", "재료는 300자 이내로 작성해주세요."),
+    Title_IS_EMPTY(HttpStatus.BAD_REQUEST, "ACCOUNT-014", "제목을 입력해주세요."),
+    Title_LONG_REQUEST(HttpStatus.BAD_REQUEST, "ACCOUNT-015", "제목은 45자 이내로 작성해주세요."),
+    AUTHOR_NOT_EQUAL(HttpStatus.BAD_REQUEST, "ACCOUNT-016", "작성자가 일치하지 않습니다."),
+    Recipe_NOT_FOUND(HttpStatus.BAD_REQUEST, "ACCOUNT-017", "레시피를 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;	// HttpStatus
