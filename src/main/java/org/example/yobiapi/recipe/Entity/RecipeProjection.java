90ff1b41c9862770849ed82f42e0c463a814615c
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-16 17:07:47 +0900

    레시피 조회를 위한 프로젝션 추가

---
+++ src/main/java/org/example/yobiapi/recipe/Entity/RecipeProjection.java
@@ -0,0 +1,17 @@
+package org.example.yobiapi.recipe.Entity;
+
+import org.example.yobiapi.user.Entity.UserProjection;
+
+import java.time.LocalDateTime;
+
+public interface RecipeProjection {
+    Integer getRecipeId();
+    UserProjection getUser();
+    String getTitle();
+    String getCategory();
+    String getIngredient();
+    Integer getViews();
+    Integer getReportCount();
+    LocalDateTime getCreateDate();
+    LocalDateTime getUpdateDate();
+}
