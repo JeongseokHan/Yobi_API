14acfcd80c1f586bb14399f8a0791108e286bc65
JeongSeokHan <wjdtjrrkt8888@gmail.com>
2024-08-21 19:37:34 +0900

    게시판 좋아요 DTO 생성

--- src/main/java/org/example/yobiapi/likes/dto/BoardLikesDTO.java
+++ src/main/java/org/example/yobiapi/likes/dto/BoardLikesDTO.java
@@ -1,4 +1,4 @@
-package org.example.yobiapi.likes;
+package org.example.yobiapi.likes.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class LikesDTO {
+public class BoardLikesDTO {
     private String userId;
     private Integer boardId;
 }
