2cbc6f6b03146b0d8aca3279d36c7ddcb19068b5
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-23 16:20:37 +0900

    댓글 레파지토리 추가

---
+++ src/main/java/org/example/yobiapi/comments/Entity/CommentsRepository.java
@@ -0,0 +1,9 @@
+package org.example.yobiapi.comments.Entity;
+
+import org.example.yobiapi.bookmark.Entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comments, Long> {
+}
