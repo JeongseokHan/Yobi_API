872d65764adc93ad7c09069bffe970906114ce85
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-24 17:00:26 +0900

    팔로우 기능 업데이트

--- src/main/java/org/example/yobiapi/follow/Entity/Follow.java
+++ src/main/java/org/example/yobiapi/follow/Entity/Follow.java
@@ -20,19 +20,18 @@ public class Follow {
     @Column(nullable = false, name = "follow_id")
     private Integer followId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "follower_id", referencedColumnName = "user_id")
-    private User followerId;
+    private User followerId; // 팔로우를 받은 사람
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "followee_id", referencedColumnName = "user_id")
-    private User followeeId;
+    private User followeeId; // 팔로우를 하는 사람
 
-    public static Follow toFollow(FollowDTO followDTO) {
+    public static Follow toFollow(User follower, User followee) {
         Follow follow = new Follow();
-        follow.followerId.setUserId(followDTO.getFollowerId());
-        follow.followeeId.setUserId(followDTO.getFolloweeId());
-
+        follow.setFollowerId(follower);
+        follow.setFolloweeId(followee);
         return follow;
     }
 }
