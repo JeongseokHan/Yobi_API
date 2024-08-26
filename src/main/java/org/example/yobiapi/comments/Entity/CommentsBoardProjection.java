3df6891af253973af0c40fce2e504c8714b35870
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-26 15:14:26 +0900

    대댓글 조회 기능 추가

--- src/main/java/org/example/yobiapi/comments/Entity/CommentsBoardProjection.java
+++ src/main/java/org/example/yobiapi/comments/Entity/CommentsBoardProjection.java
@@ -1,12 +1,11 @@
 package org.example.yobiapi.comments.Entity;
 
 import org.example.yobiapi.board.Entity.BoardIdProjection;
-import org.example.yobiapi.recipe.Entity.RecipeIdProjection;
 import org.example.yobiapi.user.Entity.UserProjection;
 
 import java.time.LocalDateTime;
 
-public interface CommentBoardProjection {
+public interface CommentsBoardProjection {
     Integer getCommentId();
     BoardIdProjection getBoard();
     UserProjection getUser();
--- src/main/java/org/example/yobiapi/comments/Entity/CommentsRepository.java
+++ src/main/java/org/example/yobiapi/comments/Entity/CommentsRepository.java
@@ -1,7 +1,6 @@
 package org.example.yobiapi.comments.Entity;
 
 import org.example.yobiapi.board.Entity.Board;
-import org.example.yobiapi.bookmark.Entity.Bookmark;
 import org.example.yobiapi.recipe.Entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +10,9 @@ import java.util.List;
 @Repository
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
     Comments findByCommentId(Integer commentId);
-    List<CommentsRecipeProjection> findAllByRecipe(Recipe recipe);
-    List<Comments> findAllByBoard(Board board);
+    List<CommentsRecipeProjection> findAllByRecipeAndCommentIdIsNull(Recipe recipe); // 레시피의 댓글 반환
+    List<CommentsBoardProjection> findAllByBoardAndCommentIdIsNull(Board board); // 게시판의 댓글 반환
+    List<CommentsRecipeProjection> findAllByRecipeAndParentCommentId(Recipe recipe, Integer parentCommentId); // 레시피의 대댓글 반환
+    List<CommentsBoardProjection> findAllByBoardAndParentCommentId(Board board, Integer parentCommentId); // 게시판의 대댓글 반환
+    List<CommentsChildProjection> findAllByParentCommentId(Integer parentCommentId); // 대댓글 반환
 }
