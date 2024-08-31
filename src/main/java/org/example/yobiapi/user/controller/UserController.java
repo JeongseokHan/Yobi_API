0ec94b4369041f5246d3d10478a13fdfef33bc3d
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-31 18:54:30 +0900

    소셜 로그인 임시 기능으로 업데이트

--- src/main/java/org/example/yobiapi/user/controller/UserController.java
+++ src/main/java/org/example/yobiapi/user/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         return ResponseEntity.status(userService.UpdateByUserNickName(updateUserNickNameDTO)).build();
     }
 
-    @PostMapping(value = "/user")
+    @PostMapping(value = "/user/1")
     public ResponseEntity<?> signIn(@RequestBody SignInDTO signInDTO) {
         return ResponseEntity.status(userService.singIn(signInDTO)).build();
     }
