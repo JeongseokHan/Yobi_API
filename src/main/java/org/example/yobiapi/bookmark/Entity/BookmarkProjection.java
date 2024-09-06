5a137dfa964d55909b3418b89dbff7dbf1a1e95f
한정석 <wjdtjrrkt8888@gmail.com>
2024-09-06 20:52:48 +0900

    else 정상화

---
+++ src/main/java/org/example/yobiapi/bookmark/Entity/BookmarkProjection.java
@@ -0,0 +1,7 @@
+package org.example.yobiapi.bookmark.Entity;
+
+import org.example.yobiapi.recipe.Entity.RecipeProjection;
+
+public interface BookmarkProjection {
+    RecipeProjection getRecipe();
+}
